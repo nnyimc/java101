0da9ea1f1dc5ac9e936d3ca46d4b6d9ab2487360
nnyimc <nyman.nicolas.auditeur@lecnam.net>
2022-02-26 15:17:39 +0100

    Completed Lab03

---
+++ AcmeOrderSystem/src/Order.java
@@ -0,0 +1,45 @@
+public class Order {
+	MyDate orderDate;
+	double orderAmount = 0.00;
+	String customer;
+	String product;
+	int quantity;
+	static double taxRate;
+	
+	static {
+		Order.taxRate = 0.05; 
+	}
+	
+	public Order(MyDate d, double amt, String c){
+		orderDate=d;
+		orderAmount=amt;
+		customer=c;
+		product = "Anvil";
+		quantity = 1;
+	}
+	
+	public Order(MyDate d, double amt, String c,String p, int q){
+		this(d, amt, c);
+		product=p;
+		quantity=q;
+	}
+	
+	
+	
+	public static void setTaxRate(double taxRate) {
+		Order.taxRate = taxRate;
+	}
+	
+	public static void computeTaxes(double amount) {
+		 System.out.println( "The tax for " + amount +  " is: " + amount * Order.taxRate);
+	}
+	
+	public double computeTaxes() {
+		System.out.println( "The tax for " +  this.orderAmount +  " is: " + orderAmount * Order.taxRate);
+		return orderAmount * Order.taxRate;
+	}
+	
+	public String toString(){
+		return quantity + " ea. " + product + " for " + customer; 
+	}
+}
---
+++ AcmeOrderSystem/src/TestOrders.java
@@ -0,0 +1,32 @@
+public class TestOrders {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MyDate date1 = new MyDate(1,20,2008);
+		Order anvilOrder = new Order(date1, 2000.00, "Wile E Coyote", "Anvil", 10);
+		System.out.println(anvilOrder);
+		anvilOrder.computeTaxes();
+
+		MyDate date2 = new MyDate(4,10,2008);
+		Order balloonsOrder = new Order(date2, 1000.00, "Bugs Bunny", "Balloon", 125);
+		System.out.println(balloonsOrder);
+		balloonsOrder.computeTaxes();
+		
+		System.out.println("The tax Rate is currently: " + Order.taxRate);
+		Order.computeTaxes(3000.00);
+		
+		
+		Order.setTaxRate(0.06);
+		System.out.println("The tax Rate is currently: " + Order.taxRate);
+		Order.computeTaxes(3000.00);
+	    anvilOrder.computeTaxes();
+		balloonsOrder.computeTaxes();
+		
+		MyDate date3 = new MyDate(5, 20, 2008);
+		Order anotherAnvil = new Order(date3, 200, "Road Runner");
+		System.out.println(anotherAnvil);
+	}
+
+}
